df55df53caff27ec4b3d5286a994df5d69f75d53
Ha <typ0@naver.com>
2017-12-20 13:47:10 +0900

    fix: refer options bug

--- src/app/livere/livere.component.ts
+++ src/app/livere/livere.component.ts
@@ -37,6 +37,7 @@ export class LivereComponent implements OnChanges, OnDestroy {
     };
 
     if (typeof window.LivereTower !== 'undefined') {
+      window.LivereTower.set({ common: this.parse() });
       return window.LivereTower.load(['comment']);
     }
 
@@ -45,6 +46,50 @@ export class LivereComponent implements OnChanges, OnDestroy {
     livereScript.async = true;
     livereScript.type = 'text/javascript';
     this.document.getElementsByTagName('head')[0].appendChild(livereScript);
+  }
+
+  isObjectPropertiesNull(a) {
+    for (const b in a) {
+      if ('string' !== typeof b || (!a[b] && 0 !== a[b])) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  parse() {
+    const a = this.document,
+      b = a.querySelector('#lv-container'),
+      c = window.livereOptions || {};
+    if (b) {
+      const d = a.querySelector('meta[property="og:image"]'),
+        e = a.querySelector('meta[property="og:title"]'),
+        f = a.querySelector('meta[property="og:url"]'),
+        g = location.hash,
+        h = g.match(/#highlight-comment=([0-9]{1,})/),
+        i: any = {
+          id: b.getAttribute('data-id'),
+          refer: c.refer || window.refer || location.host + location.pathname,
+        };
+      if (!this.isObjectPropertiesNull(i)) {
+        return (
+          (i.uid = b.getAttribute('data-uid') || null),
+          (i.site = c.site || (f ? f.getAttribute('content') : location.href)),
+          (i.title =
+            (e ? e.getAttribute('content') : document.title) ||
+            'Welcome to the black parade'),
+          (i.logo = d ? d.getAttribute('content') : null),
+          (i.highlightSeq = h ? h[h.length - 1] : null),
+          (i.redirectOrigin = c.redirectOrigin || null),
+          (i.title = i.title.replace(/\"/g, '＂').replace(/\'/g, '＇')),
+          c.facebookPageId &&
+            c.facebookUploadUrl &&
+            ((i.facebookPageId = c.facebookPageId),
+            (i.facebookUploadUrl = c.facebookUploadUrl)),
+          i
+        );
+      }
+    }
   }
 
   ngOnDestroy() {
